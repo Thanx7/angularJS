ba27cbb853cbc826bc8d86be46d37669f3a6e774
Thanx7 <Aliaksandr_Doubeikovsky@epam.com>
2014-09-08 16:07:12 +0300

    1

---
+++ namesController.js
@@ -0,0 +1,8 @@
+function namesController($scope) {
+    $scope.names = [
+        {name:'Jani', city:'Berlin', country:'Germany', age: '20'},
+        {name:'Hege', city:'Graz', country:'Austria', age: '30'},
+        {name:'Kai', city:'Minsk', country:'Belarus', age: '40'},
+        {name:'Ann', city:'Paris', country:'France', age: '50'}
+    ];
+}
